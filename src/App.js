e3f4882402524a86c46d7caddecd07e525e86788
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-08-18 09:33:34 +0400

    get the ownprops in mapStateToProps function and use it

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import store from "./redux/store";
 function App() {
   return (
     <Provider store={store}>
-      <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-        <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+      <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
           Simple Counter Application
         </h1>
 
-        <div class="max-w-md mx-auto mt-10 space-y-5"></div>
-        <Counter />
+        <div className="max-w-md mx-auto mt-10 space-y-5"></div>
+        <Counter name="counter" />
       </div>
     </Provider>
   );
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,21 @@
 import { connect } from "react-redux";
 import { decrement, increment } from "../redux/counter/actions";
 
-const Counter = ({ count, incrementHandler, decrementHandler }) => {
+const Counter = ({ count, incrementHandler, decrementHandler, name }) => {
   return (
-    <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-      <div class="text-2xl font-semibold">{count}</div>
-      <div class="flex space-x-3">
+    <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+      <div className="text-2xl font-semibold">
+        {count} - {name}
+      </div>
+      <div className="flex space-x-3">
         <button
-          class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+          className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
           onClick={() => incrementHandler(5)}
         >
           Increment
         </button>
         <button
-          class="bg-red-400 text-white px-3 py-2 rounded shadow"
+          className="bg-red-400 text-white px-3 py-2 rounded shadow"
           onClick={() => decrementHandler(2)}
         >
           Decrement
@@ -23,7 +25,8 @@ const Counter = ({ count, incrementHandler, decrementHandler }) => {
   );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+  console.log(ownProps);
   return {
     count: state.value,
   };
