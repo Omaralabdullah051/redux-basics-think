9e5cd22cf9b9b5fb1115ee2fa2e73ca6db4de67e
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-08-18 09:40:59 +0400

    replace connect API with useSelector and useDispatch hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Provider } from "react-redux";
-import Counter from "./components/Counter";
+import HookCounter from "./components/HookCounter";
+// import Counter from "./components/Counter";
 import store from "./redux/store";
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
         </h1>
 
         <div className="max-w-md mx-auto mt-10 space-y-5"></div>
-        <Counter name="counter" />
+        {/* <Counter name="counter" /> */}
+        <HookCounter />
       </div>
     </Provider>
   );
